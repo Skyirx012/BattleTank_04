04b1f9660868447ab1d407e19d8b3983f045b505
John-PC\John <jk-012@hotmail.com>
2018-05-02 20:24:21 -0600

    BT17 Created Tick method and foundation for aiming at crosshair

--- BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -19,6 +19,10 @@ public:
 
 	ATank* GetControlledTank() const;
 	
-	void BeginPlay() override;
-	
+	virtual void BeginPlay() override;
+
+	virtual void Tick(float DeltaTime) override;
+
+	//Start the tank moving the barrel so that a shot would hit where the crosshair intersects with the world
+	void AimTowardsCrosshair();
 };
